727cf5914114dcb26502cd45184666da2b7784c2
Douglas Bernardes <douglaspb96@gmail.com>
2020-04-22 00:45:34 -0300

    Adicionada nova build

---
+++ build/precache-manifest.333f23c2024ea30a66265772e7e9afc6.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "925a6f871ce55e3f51d6c4e09e9b68dc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dc60b981048ba6b2f206",
+    "url": "/static/css/main.de424728.chunk.css"
+  },
+  {
+    "revision": "9d6521dcc3f06eb6c75c",
+    "url": "/static/js/2.b836c8e2.chunk.js"
+  },
+  {
+    "revision": "961d1a50ea5da5f0ed0257d219c32932",
+    "url": "/static/js/2.b836c8e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc60b981048ba6b2f206",
+    "url": "/static/js/main.0f6b00cd.chunk.js"
+  },
+  {
+    "revision": "69919ae5135229cce8a3",
+    "url": "/static/js/runtime-main.88f82d7a.js"
+  },
+  {
+    "revision": "b2102722b8cdc629621049941b9664a6",
+    "url": "/static/media/Another_Flight.b2102722.otf"
+  },
+  {
+    "revision": "506962498224901493c0880fed0600b0",
+    "url": "/static/media/logo.50696249.png"
+  },
+  {
+    "revision": "f98b32ceb4e65ec11ae685fc4b6d5a15",
+    "url": "/static/media/no-image.f98b32ce.png"
+  },
+  {
+    "revision": "b0dc278f6157427eab8f0a70dbd56654",
+    "url": "/static/media/no-thumbnail.b0dc278f.png"
+  },
+  {
+    "revision": "5481a621d2381be2a6c2f423776aeae9",
+    "url": "/static/media/secondImage.5481a621.png"
+  }
+]);
