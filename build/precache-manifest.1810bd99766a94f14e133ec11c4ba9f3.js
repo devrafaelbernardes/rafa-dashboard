8d9d08a5bc33e5d70cdc3d7ed655caa4de011e10
Douglas Bernardes <douglaspb96@gmail.com>
2020-11-26 14:01:48 -0300

    Download da font principal e corrigido style do componente de remoçao

---
+++ build/precache-manifest.1810bd99766a94f14e133ec11c4ba9f3.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33fdce80636dffea5e2937cad2ec2115",
+    "url": "/index.html"
+  },
+  {
+    "revision": "31ab230bbf2151a22367",
+    "url": "/static/css/main.de424728.chunk.css"
+  },
+  {
+    "revision": "a012ca1d41b99eb090f0",
+    "url": "/static/js/2.1f3ba593.chunk.js"
+  },
+  {
+    "revision": "ee412ffc07b10f8a24b8dde337b433e8",
+    "url": "/static/js/2.1f3ba593.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31ab230bbf2151a22367",
+    "url": "/static/js/main.8ee6a6eb.chunk.js"
+  },
+  {
+    "revision": "faaae247f2136725277e",
+    "url": "/static/js/runtime-main.dd85382f.js"
+  },
+  {
+    "revision": "b2102722b8cdc629621049941b9664a6",
+    "url": "/static/media/Another_Flight.b2102722.otf"
+  },
+  {
+    "revision": "b359a7e2080fb171a4befe467e647482",
+    "url": "/static/media/NEOTERIC.b359a7e2.ttf"
+  },
+  {
+    "revision": "506962498224901493c0880fed0600b0",
+    "url": "/static/media/logo.50696249.png"
+  },
+  {
+    "revision": "f98b32ceb4e65ec11ae685fc4b6d5a15",
+    "url": "/static/media/no-image.f98b32ce.png"
+  },
+  {
+    "revision": "b0dc278f6157427eab8f0a70dbd56654",
+    "url": "/static/media/no-thumbnail.b0dc278f.png"
+  },
+  {
+    "revision": "5481a621d2381be2a6c2f423776aeae9",
+    "url": "/static/media/secondImage.5481a621.png"
+  }
+]);
