bf79b03aeaa528efa97a827c466beaf4383e3b1f
Stokei Oficial <stokeioficial@gmail.com>
2022-01-20 18:45:51 -0300

    chager media styles

--- src/components/Media/styles.js
+++ src/components/Media/styles.js
@@ -1,9 +1,11 @@
 import styled from 'styled-components';
 import { Row } from 'react-bootstrap';
-import ImageDefault from 'components/Image';
+import ImageDefault from '../Image';
 
 export const Container = styled(Row)`
     width: 100% !important;
+    display: flex;
+    flex-direction: column;
 `;
 
 export const Image = styled(ImageDefault)`
@@ -14,6 +16,7 @@ export const Title = styled(Row)`
     width: 100% !important;
     font-size: 25px;
     color: ${({ theme }) => theme.colors.text_dark};
+    font-family: ${({ theme }) => theme.fonts.secondary};
     text-align: center;
     text-transform: uppercase;
 `;
